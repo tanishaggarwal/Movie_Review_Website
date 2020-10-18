0b38f96d1fcf5613a02f84a361dce38cdeef147a
tanishaggarwal <tanishaggarwal9357@gmail.com>
2020-10-18 13:34:08 +0530

    delete

---
+++ DeleteAccount.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Movie_Review_Website
+{
+    public partial class DeleteAccount : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            String username = tbUsername.Text.ToString();
+            String password = tbPassword.Text.ToString();
+            String connectionstring = ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionstring);
+            conn.Open();
+            SqlCommand cm = conn.CreateCommand();
+            cm.CommandType = CommandType.Text;
+            cm.CommandText = "select password from userdata where username = @u";
+            cm.Parameters.AddWithValue("@u", username);
+            lError.Visible = true;
+            try
+            {
+                SqlDataReader reader = cm.ExecuteReader();
+                reader.Read();
+                String retrievedPassword = reader["password"].ToString();
+                if (retrievedPassword.Equals(password))
+                {
+                    reader.Close();
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "delete from userdata where username = @u";
+                    cmd.Parameters.AddWithValue("@u", username);
+                    cmd.ExecuteNonQuery();
+                    lError.Text = "Account Deleted Successfully";
+                    tbUsername.Text = "";
+                   // ButtonLogin.Visible = false;
+                }
+                else
+                {
+                    lError.Text = "*Username or Password is incorrect";
+                }
+            }
+            catch (Exception E)
+            {
+                lError.Text = "*Username or Password is incorrect";
+            }
+        }
+    }
+}
